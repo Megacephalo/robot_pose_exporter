c0d161b107605840d65f6f6c560bea9fe880e555
Charly Huang <charly.charlongo@gmail.com>
2019-10-16 17:18:27 -0400

    Introduce save_odom_to_csv functionality

--- scripts/write_to_csv_lib/write_to_csv.py
+++ scripts/write_to_csv_lib/write_to_csv.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose, PoseStamped
 import csv
 import datetime
 from tf import transformations
 
 class writeToCSV:
-    def __init__(self, output_fileName):
+    def __init__(self, output_fileName, poseMode = 'Pose'):
         iso_time = datetime.datetime.now().replace(microsecond=0).isoformat()
         self.output_fileName = str( output_fileName ) + '_' + str( iso_time )
         if not self.output_fileName.lower().endswith('.csv'):
@@ -16,14 +16,18 @@ class writeToCSV:
         # Insert the metadata
         with open( self.output_fileName, mode = 'w' ) as outFile:
             poseWriter = csv.writer(outFile, delimiter = ',')
-            poseWriter.writerow( ['timestamp', 'x(m)', 'y(m)', 'z(m)', 'orientation (rad)'] )
+
+            if poseMode == 'Pose':
+                poseWriter.writerow( ['x(m)', 'y(m)', 'z(m)', 'orientation (rad)'] )
+            elif poseMode == 'PoseStamped':
+                poseWriter.writerow( ['timestamp', 'x(m)', 'y(m)', 'z(m)', 'orientation (rad)'] )
 
         if rospy.is_shutdown():
             print( '{} created'.format(self.output_fileName) )
         else:
             rospy.loginfo( '{} created'.format(self.output_fileName) )
 
-    def outputPoseToCSV(self, poseStamped):
+    def outputPoseStampedToCSV(self, poseStamped):
         if not self.output_fileName:
             err_msg = 'There is no specified outputfile thus nowhere to write. Quitting.'
             if rospy.is_shutdown():
@@ -50,3 +54,32 @@ class writeToCSV:
 
             poseWriter = csv.writer(outFile, delimiter = ',')
             poseWriter.writerow( [timestamp, x, y, z, yaw] )
+        
+    def savePoseToCSV(self, pose):
+        if not self.output_fileName:
+            err_msg = 'There is no specified outputfile thus nowhere to write. Quitting.'
+            if rospy.is_shutdown():
+                print(err_msg)
+            else:
+                rospy.logerr(err_msg)
+            return
+        
+        with open( self.output_fileName, mode = 'a' ) as outFile:
+            new_pose = pose
+            x = new_pose.position.x
+            y = new_pose.position.y
+            z = new_pose.position.z
+
+            quaternion = (
+                new_pose.orientation.x,
+                new_pose.orientation.y,
+                new_pose.orientation.z,
+                new_pose.orientation.w
+            )
+            euler = transformations.euler_from_quaternion( quaternion )
+            roll, pitch, yaw = euler[0], euler[1], euler[2]
+
+            poseWriter = csv.writer(outFile, delimiter = ',')
+            poseWriter.writerow( [x, y, z, yaw] )
+
+
